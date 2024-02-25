32cc5b733acb1d7cbfd34d70a46a0eda3b7bec1e
Alexandre <alexandre.acustica@gmail.com>
2024-02-25 06:25:08 -0300

    static import maps

--- root/webpack.config.js
+++ root/webpack.config.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require("path");
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "plansul";
   const defaultConfig = singleSpaDefaults({
@@ -14,7 +14,16 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     devServer: {
-      static: "src",
+      static: [
+        {
+          directory: path.join(__dirname, "assets", "libs"), // Caminho para a pasta assets/libs
+          publicPath: "/assets/libs/", // Caminho público para a pasta
+        },
+        {
+          directory: path.join(__dirname, "assets", "maps"), // Caminho para a pasta assets/libs
+          publicPath: "/assets/maps/", // Caminho público para a pasta
+        },
+      ],
     },
     // modify the webpack config however you'd like to by adding to this object
     plugins: [
